41aa81d7191ba07f3a05f0d7242655e798d1fc2a
lucasraggi89 <lucas.raggi@hotmail.com>
2018-12-18 11:54:13 -0300

    Code commented and temp.json deleted

--- v3-mining.py
+++ v3-mining.py
@@ -1,25 +1,33 @@
 import requests
-import json
 import logging
 
 
 def get_comment_from_issue(issue, params, username, token):
     comment_res = requests.get(issue['comments_url'], params=params, auth=(username, token))
-    comments = comment_res.json()
-    print('     Comments from: ' + str(issue['id']))
-    for comment in comments:
-        print('             Comment ID: ' + str(comment['id']))
+    if comment_res.ok:
+        comments = comment_res.json()
+        print('     Comments from: ' + str(issue['id']))
+        for comment in comments:
+            print('             Comment ID: ' + str(comment['id']))
+    else:
+        logging.warning(str(comment_res.status_code))
 
 
+# Return False if issue does not have a closed event
+# Return login of the person who closed the issue if the issue has a closed event
 def is_event_closed(issue, params, username, token):
     event_res = requests.get(issue['events_url'], params=params, auth=(username, token))
-    events = event_res.json()
-    for event in events:
-        if event['event'] == 'closed':
-            return event['actor']['login']
-    return False
+    if event_res.ok:
+        events = event_res.json()
+        for event in events:
+            if event['event'] == 'closed':
+                return event['actor']['login']
+        return False
+    else:
+        logging.warning(str(event_res.status_code))
 
 
+# Get issues from all pages from selected repository
 def get_all_pages(res, issues, params, username, token):
     count = 0
     while 'next' in res.links.keys():
@@ -30,10 +38,11 @@ def get_all_pages(res, issues, params, username, token):
     return issues
 
 
+# Get comments from issues with the word "bug" in the label
 def get_all_bug_issues(issues, params, username, token):
     for issue in issues:
         for label in issue['labels']:
-            closed_by = is_event_closed(issue, params, username, token)
+            closed_by = is_event_closed(issue, params, username, token) #
             if label['name'].find("bug") != -1 and closed_by is not False:
                 print('Issue title: ' + issue['title'])
                 print('     Opened By: ' + issue['user']['login'])
@@ -43,6 +52,7 @@ def get_all_bug_issues(issues, params, username, token):
                 get_comment_from_issue(issue, params, username, token)
 
 
+# Debug function to match issues from web clients not including pull requests
 def get_all_issues_debug(issues, params, username, token):
     for issue in issues:
         for label in issue['labels']:
@@ -61,14 +71,14 @@ def main():
     params = {'state': 'closed', 'per_page': '100'}
 
     username = 'lucas.raggi@hotmail.com'
-    token = '232d08ee5274613f1d8f063abaa7fded8656fd6b'
+    token = '232d08ee5274613f1d8f063abaa7fded8656fd6b' # Token with permission only for requests
 
     res = requests.get(url, params=params, auth=(username, token))
     if res.ok:
         issues = res.json()
         # issues = get_all_pages(res, issues, params, username, token)
-        get_all_issues_debug(issues, params, username, token)
-        # get_all_bug_issues(issues, params, username, token)
+        # get_all_issues_debug(issues, params, username, token)
+        get_all_bug_issues(issues, params, username, token)
     else:
         logging.warning(str(res.status_code))
 
