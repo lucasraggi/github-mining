68618dd32e83114f5a3ae3fdfa67923250ca65d4
lucas <lucas.raggi@hotmail.com>
2018-12-20 00:25:48 -0200

    collecting closed issues with label bug that aren't a pull request instead of all issues

--- src/miner/issue_miner.py
+++ src/miner/issue_miner.py
@@ -1,7 +1,7 @@
 import errno
 import json
 import os
-
+from ast import literal_eval
 import requests
 import logging
 
@@ -21,58 +21,13 @@ class IssueMiner:
         self.events_output_path = issues_events_output_path
         self.username = username
         self.token = token
-        self.params = params
+        self.params = literal_eval(params)
 
         self.closed_issues = {}
         self.closed_issues_numbers = []
         self.closed_issues_events = {}
         self.closed_issues_comments = {}
         self.base_url = 'https://api.github.com/repos/'
-
-    def get_key_from_issue(self, issue, key):
-        key_res = requests.get(issue[key], params=self.params, auth=(self.username, self.token))
-        if key_res.ok:
-            keys = key_res.json()
-            return keys
-        else:
-            logging.warning(str(key_res.status_code))
-
-    # Get issues from all pages from selected repository
-    def get_all_pages(self, res, issues):
-        count = 0
-        while 'next' in res.links.keys():
-            logging.info('Retrieving page ' + count)
-            res = requests.get(res.links['next']['url'], params=self.params, auth=(self.username, self.token))
-            issues.extend(res.json())
-            count += 1
-        return issues
-
-    def get_all_issues(self, issues):
-        logging.info('Mining issues, events and comments...')
-        print('Mining issues, events and comments...')
-        for issue in issues:
-            issue_events = self.get_key_from_issue(issue, 'events_url')
-            issue_comments = self.get_key_from_issue(issue, 'comments_url')
-
-            self.closed_issues_numbers.append(issue['number'])
-            self.closed_issues[str(issue['number'])] = issue
-            self.closed_issues_events[str(issue['number'])] = issue_events
-            self.closed_issues_comments[str(issue['number'])] = issue_comments
-        logging.info('Github issues, events and comments successfully mined...')
-        print('Github issues, events and comments successfully mined...')
-        self.save_json('issues')
-        self.save_json('events')
-        self.save_json('comments')
-
-    def mine_issues(self):
-        res_url = self.base_url + self.url + "/issues"
-        res = requests.get(res_url, params=self.params, auth=(self.username, self.token))
-        if res.ok:
-            issues = res.json()
-            # issues = get_all_pages(res, issues, params, username, token)
-            self.get_all_issues(issues)
-        else:
-            logging.warning(str(res.status_code))
 
     def save_json(self, json_type):
         path, json_list = self.path_and_data_by_type(json_type)
@@ -98,6 +53,77 @@ class IssueMiner:
             return self.events_output_path, self.closed_issues_events
         if json_type == 'comments':
             return self.comments_output_path, self.closed_issues_comments
+
+    def get_key_from_issue(self, issue, key):
+        key_res = requests.get(issue[key], params=self.params, auth=(self.username, self.token))
+        if key_res.ok:
+            keys = key_res.json()
+            return keys
+        else:
+            logging.warning(str(key_res.status_code))
+
+    # Get issues from all pages from selected repository
+    def get_all_pages(self, res, issues):
+        count = 0
+        while 'next' in res.links.keys():
+            logging.info('Retrieving page ' + count)
+            res = requests.get(res.links['next']['url'], params=self.params, auth=(self.username, self.token))
+            issues.extend(res.json())
+            count += 1
+        return issues
+
+    def is_issue_closed(self, issue):
+        closed_count = 0
+        reopened_count = 0
+        event_res = requests.get(issue['events_url'], params=self.params, auth=(self.username, self.token))
+        if event_res.ok:
+            events = event_res.json()
+            for event in events:
+                if event['event'] == 'closed':
+                    closed_count += 1
+                if event['event'] == 'reopened':
+                    reopened_count += 1
+            if closed_count > reopened_count:
+                return True
+            return False
+        else:
+            logging.warning(str(event_res.status_code))
+
+    def get_all_issues(self, issues):
+        logging.info('Mining issues, events and comments...')
+        print('Mining issues, events and comments...')
+        for issue in issues:
+            # print('issue title: ', issue['title'])
+            for label in issue['labels']:
+                if 'pull_request' not in issue and label['name'].find("bug") != -1 and self.is_issue_closed(issue):
+                    print('label name: ', label['name'])
+                    print('label[name].find(bug): ', label['name'].find("bug"))
+                    print('number: ', issue['number'])
+                    issue_events = self.get_key_from_issue(issue, 'events_url')
+                    issue_comments = self.get_key_from_issue(issue, 'comments_url')
+
+                    self.closed_issues_numbers.append(issue['number'])
+                    self.closed_issues[str(issue['number'])] = issue
+                    self.closed_issues_events[str(issue['number'])] = issue_events
+                    self.closed_issues_comments[str(issue['number'])] = issue_comments
+        logging.info('Github issues, events and comments successfully mined...')
+        print('Github issues, events and comments successfully mined...')
+        self.save_json('issues')
+        self.save_json('events')
+        self.save_json('comments')
+
+    def mine_issues(self):
+        res_url = self.base_url + self.url + "/issues"
+        print('params: ', self.params)
+        res = requests.get(res_url, params=self.params, auth=(self.username, self.token))
+        if res.ok:
+            issues = res.json()
+            # issues = get_all_pages(res, issues, params, username, token)
+            self.get_all_issues(issues)
+        else:
+            logging.warning(str(res.status_code))
+
+
 
 
 
